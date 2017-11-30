bbc436c9c1b8da1af29617fb5298a6ad57a73834
bdoobie01 <brian.dubb01@gmail.com>
2017-11-30 17:15:57 -0600

    s

---
+++ Header.java
@@ -0,0 +1,16 @@
+/* Chat Program <MyClass.java>
+ * EE422C Project 7 submission by
+ * Replace <...> with your actual data.
+ * <Student1 Name>
+ * <Student1 EID>
+ * <Student1 5-digit Unique No.>
+ * <Student2 Name>
+ * <Student2 EID>
+ * <Student2 5-digit Unique No.>
+ * Group on Canvas:
+ * Slip days used: <0>
+ * Fall 2017
+ * GitHub URL:
+ */
+
+package assignment7;
